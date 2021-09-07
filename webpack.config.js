a9fdf23bb15ec57d6a313e77e7e32886f80b07ac
Mariana Cavalcante <marianacavalcante.nasc@gmail.com>
2021-09-07 18:20:59 -0300

    Configurando scss

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,11 @@ module.exports = {
         exclude: /node_modules/,
         use: "babel-loader",
       },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
 };

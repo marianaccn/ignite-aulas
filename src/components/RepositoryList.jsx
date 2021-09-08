d2b80b5bc143cde3d71d0ade74cacbbcb292924b
Mariana Cavalcante <marianacavalcante.nasc@gmail.com>
2021-09-08 12:31:03 -0300

    Aprendendo a usar o useEffect e listando repositorios da API

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,26 @@
 import { RepositoryItem } from "./RespositoryItem";
 import "../styles/repositories.scss";
-
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/rocketseat",
-};
+import { useEffect, useState } from "react";
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repostirórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
       </ul>
     </section>
   );
--- src/components/RespositoryItem.jsx
+++ src/components/RespositoryItem.jsx
@@ -1,9 +1,9 @@
 export function RepositoryItem(props) {
   return (
     <li>
-      <strong>{props.repository.name ?? "Título"}</strong>
-      <p>{props.repository.description ?? "Descrição"}</p>
-      <a href={props.repository.link}>Acessar repositórios</a>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
+      <a href={props.repository.html_url}>Acessar repositórios</a>
     </li>
   );
 }
